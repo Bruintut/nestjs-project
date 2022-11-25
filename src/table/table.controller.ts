92f91ecefaf58ebb98f9f40b5a140c866f7b3c99
Bruno Oliveira <105464045+Bruintut@users.noreply.github.com>
2022-11-25 15:47:00 -0300

    feat: add create

--- src/table/table.controller.ts
+++ src/table/table.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Post } from "@nestjs/common"
+import { Controller, Get, Post, Body } from "@nestjs/common"
+import { CreateTableDto } from "./Dto/crreate-table.dto"
 import { TableService } from "./table.service"
 
 @Controller('table')
@@ -11,7 +12,7 @@ export class TableController {
     }
 
     @Post()
-    create(){
-        return this.tableService.create()
+    create(@Body() createTableDto: CreateTableDto){
+        return this.tableService.create(createTableDto)
     }
 }
--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { CreateTableDto } from './Dto/crreate-table.dto';
+import { Table } from './entities/table.entity';
 
 @Injectable()
 export class TableService {
+  tables: Table[] = [];
   findAll() {
     return 'Find All';
   }
-  create() {
-    return 'create';
+  create(createTableDto: CreateTableDto) {
+    const table: Table = {id: 'random_id', ...createTableDto};
+    this.tables.push(table);
+    return table;
   }
 }
